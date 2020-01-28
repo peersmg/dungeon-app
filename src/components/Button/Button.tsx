713654ffa27144246c48936db4231e2c222fb6ef
Matthew Peers <peersmg@gmail.com>
2020-01-28 12:24:23 +0000

    [Matt] Adds gitignore for vscode

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 
 interface ButtonProp {
-  buttonCallback?: (() => void);
+  buttonCallback?: () => void;
 }
 
-const Button: React.FC<ButtonProp> = (props) => {
-
+const Button: React.FC<ButtonProp> = props => {
   function buttonClicked() {
-    if(props.buttonCallback) {
+    if (props.buttonCallback) {
       props.buttonCallback();
     }
   }
 
-    return (
-    <button onClick={buttonClicked}>{props.children}</button>
-    );
-  }
-  
-  export default Button;
+  return <button onClick={buttonClicked}>{props.children}</button>;
+};
+
+export default Button;
