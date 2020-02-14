f5cfcde49070d23fa71a60002720888b4e717efd
peersmg <peersmg@gmail.com>
2020-02-14 12:53:55 +0000

    [Matt][#26] Makes 3D canvas load from redux

--- src/Game/Engine/canvas/Canvas3D.ts
+++ src/Game/Engine/canvas/Canvas3D.ts
@@ -3,12 +3,15 @@ import Tile2D from "../Utils/Tile2D";
 import ICamera from "../camera/ICamera";
 import * as THREE from "three";
 import Camera3D from "../camera/Camera3D";
+import dataStore from "../../../redux/store";
+import { EnvironmentType } from "../../TileTypes";
 
 class Canvas3D implements ICanvas {
   private _camera: ICamera;
   private _scene: THREE.Scene;
   private _renderer: THREE.WebGLRenderer;
   private _containerElement: HTMLElement | null;
+  private _initialised: boolean = false;
 
   constructor(camera: ICamera) {
     this._camera = camera;
@@ -29,21 +32,68 @@ class Canvas3D implements ICanvas {
   }
 
   render(): void {
+    if (!this._initialised) {
+      // Add environment
+      this.addEnvironment();
+
+      // Add entities
+      this.addEntities();
+
+      this._initialised = true;
+    }
+
     this._renderer.render(this._scene, (this._camera as Camera3D).get3DCamera());
   }
 
-  addTile(newBox: Tile2D | null): void {
-    if (newBox) {
-      let geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-      let material = new THREE.MeshNormalMaterial();
-      let mesh = new THREE.Mesh(geometry, material);
-      mesh.position.set(
-        (newBox.position.x / 52) * 0.05 - 2,
-        -(newBox.position.y / 52) * 0.05 + 2,
-        newBox.zLevel * 0.05
-      );
-      this._scene.add(mesh);
+  private addEntities() {
+    let entityArray = dataStore.getState().map.entities;
+    console.log("Adding entities:");
+    for (let i = 0; i < entityArray.length; i++) {
+      console.log("Adding entity");
+      this.addCube(entityArray[i].mapCoord.x, entityArray[i].mapCoord.y, 1);
     }
+  }
+
+  private addEnvironment() {
+    let envArray = dataStore.getState().map.environment;
+
+    for (let x = 0; x < envArray[0].length; x++) {
+      for (let y = 0; y < envArray.length; y++) {
+        let envType = dataStore
+          .getState()
+          .map.environmentTypes.find(val => val.id == envArray[y][x]);
+
+        if (envType) {
+          this.addCube(x, y, envType.zLevel);
+        }
+      }
+    }
+  }
+
+  private addCube(x: number, y: number, zLevel: number) {
+    let xOffset = -1;
+    let yOffset = 0.5;
+    let boxSize = 0.05;
+
+    let geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x * boxSize + xOffset, -y * boxSize + yOffset, zLevel * boxSize);
+    this._scene.add(mesh);
+  }
+
+  addTile(newBox: Tile2D | null): void {
+    // if (newBox) {
+    //   let geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+    //   let material = new THREE.MeshNormalMaterial();
+    //   let mesh = new THREE.Mesh(geometry, material);
+    //   mesh.position.set(
+    //     (newBox.position.x / 52) * 0.05 - 2,
+    //     -(newBox.position.y / 52) * 0.05 + 2,
+    //     newBox.zLevel * 0.05
+    //   );
+    //   this._scene.add(mesh);
+    // }
   }
   removeTile(tileToRemove: Tile2D | null): void {
     //throw new Error("Method not implemented.");
